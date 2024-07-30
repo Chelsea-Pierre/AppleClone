bcd9358bb26da892bb2781a84d176e4b0a7a0010
Chelsea Pierre <chelseapierre82@yahoo.com>
2024-07-29 23:27:12 -0400

    added additonal folders and created files for app.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { appleImg } from '../utils';
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src={appleImg} alt="Apple" width={14} height={18}/>
+                <div> 
+                    {}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
